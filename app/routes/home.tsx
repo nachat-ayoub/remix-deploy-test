10165c84d236ce009942248d2ae079d98aac4dad
nachat-ayoub <venomx2020x@gmail.com>
2023-12-18 16:01:24 +0100

    New Commit

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,22 +3,23 @@ import { Link } from '@remix-run/react';
 
 export const meta: MetaFunction = () => {
   return [
-    { title: 'Anime4World | Welcom' },
-    { name: 'description', content: 'Welcome to Anime4World!' },
+    { title: 'Anime4World | Home' },
+    { name: 'description', content: 'Welcome to Anime4World Home!' },
   ];
 };
 
 export default function Index() {
   return (
     <center style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
-      <h1>Anime4World Welcom Page</h1>
+      <h1>Anime4World Home Page</h1>
       <img
         height={'340px'}
-        src='https://pngfre.com/wp-content/uploads/anime-33-759x1024.png'
-        alt='Anime Image'
+        src='https://i.pngimg.me/thumb/f/720/comdlpng6937049.jpg'
+        alt='Anime4World'
       />
+
       <h3>
-        <Link to='/home'>Home Page</Link>
+        <Link to='/'>Welcom Page</Link>
       </h3>
     </center>
   );
