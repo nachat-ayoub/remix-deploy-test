a35081c8fcef068554f6537d710b908b0342aee9
nachat-ayoub <venomx2020x@gmail.com>
2023-12-18 16:31:59 +0100

    add anothe route

---
+++ app/routes/home/search/$name.tsx
@@ -0,0 +1,27 @@
+import type { MetaFunction } from '@remix-run/node';
+import { Link } from '@remix-run/react';
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: 'Anime4World | Search' },
+    { name: 'description', content: 'Search in Anime4World' },
+  ];
+};
+
+export default function Index() {
+  return (
+    <center style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.8' }}>
+      <h1>Anime4World Search Page</h1>
+      <img
+        height={'340px'}
+        src='https://pngfre.com/wp-content/uploads/anime-33-759x1024.png'
+        alt='Anime Image'
+      />
+      <h3>
+        <Link to='/home/animes' prefetch='intent'>
+          Home Page
+        </Link>
+      </h3>
+    </center>
+  );
+}
